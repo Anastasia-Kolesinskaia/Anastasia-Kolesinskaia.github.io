f21ed06af2df952e8657203cd0cd23f907137723
Anastasia-Kolesinskaia <ligntclover@gmail.com>
2022-06-09 11:44:55 +0200

    Анимация на мобильных устройствах

--- journal/assets/script.js
+++ journal/assets/script.js
@@ -18,20 +18,28 @@ bookmarks.forEach((mark, index) => {
 });
 
 const main = document.querySelector('main');
+const sections = document.querySelectorAll('section');
 const html = document.querySelector('html');
 const body = document.querySelector('body');
 const colors = ['#00c1b5','#ff651a','#ffbe00','#1d3fbb','#e30512'];
 
-const scrollEvent = () => {
-    const sections = document.querySelectorAll('section');
-  
+const scrollMain = () => scrollEvent(main.scrollTop);
+const scrollWindow = () => scrollEvent(document.documentElement.scrollTop);
+
+const scrollEvent = (scroll) => {
     let sectionHeight = 684;
-    let sectionNumber = Math.floor(1 + ((main.scrollTop - sectionHeight/2) / sectionHeight));
+    let sectionNumber = Math.floor(1 + ((scroll - sectionHeight/2) / sectionHeight));
+    
     selectedMark = sectionNumber;
+    
+    if (selectedMark >= sections.length)
+        selectedMark = sections.length - 1
+
     reset();
 
     let colorPage = colors[selectedMark];
     html.style.backgroundColor = colorPage;
 };
 
-main.addEventListener('scroll', scrollEvent);
+main.addEventListener('scroll', scrollMain);
+window.addEventListener("scroll", scrollWindow);
